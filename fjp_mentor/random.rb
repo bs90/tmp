30abfd87c63a1841cb0c192e70d6e4f50d8cc960
bs90 <bs90@TT-2.local>
2018-08-04 10:24:21 +0900

    random mentors and mentees

---
+++ fjp_mentor/random.rb
@@ -0,0 +1,61 @@
+data = [
+["水谷 健太","Japan branch","Hanoi","2年4ヶ月","","◯","https://appdata.chatwork.com/avatar/2248/2248318.rsz.jpg"],
+["田上 まり","Backoffice","Kanda","2年1ヶ月","","◯","https://appdata.chatwork.com/avatar/367/367476.rsz.jpg"],
+["向井 麻也","Backoffice","Kanda","0年1ヶ月","","◯","https://appdata.chatwork.com/avatar/2509/2509428.rsz.png"],
+["越本 理恵","Backoffice","Hanoi","1年4ヶ月","","◯","https://appdata.chatwork.com/avatar/2554/2554864.png"],
+["越本 勇一","Backoffice","HCM","0年8ヶ月","","◯","https://appdata.chatwork.com/avatar/2080/2080748.rsz.jpg"],
+["大西 健資","Sales","Kanda","3年6ヶ月","◯","","https://appdata.chatwork.com/avatar/2374/2374153.rsz.jpg"],
+["住山 省太","Sales","Kanda","2年10ヶ月","◯","","https://appdata.chatwork.com/avatar/2516/2516057.rsz.jpg"],
+["熊野 美帆","Sales","Kanda","2年6ヶ月","","◯","https://appdata.chatwork.com/avatar/2431/2431123.rsz.png"],
+["Nguyen Thi Hien","Sales","Kanda","0年4ヶ月","","◯","https://appdata.chatwork.com/avatar/2232/2232887.png"],
+["中島 章吾","Sales","Kanda","0年3ヶ月","","◯","https://appdata.chatwork.com/avatar/2433/2433907.rsz.jpg"],
+["本間 紀史","Consul","Kanda","3年7ヶ月","◯","","https://appdata.chatwork.com/avatar/2450/2450961.png"],
+["石川 公平","Consul","Kanda","0年11ヶ月","◯","","https://appdata.chatwork.com/avatar/1910/1910057.png"],
+["木嵜 雅也","Consul","Kanda","2年8ヶ月","◯","","https://appdata.chatwork.com/avatar/926/926521.rsz.png"],
+["市川 早真","Consul","Kanda","3年1ヶ月","","◯","https://appdata.chatwork.com/avatar/1230/1230874.png"],
+["角 勝平","Consul","Kanda","2年8ヶ月","","◯","https://appdata.chatwork.com/avatar/2517/???.png"],
+["呂 相吾","Consul","Kanda","0年5ヶ月","◯","","https://appdata.chatwork.com/avatar/2517/2517370.png"],
+["梅田 琢也","Consul","Kanda","0年3ヶ月","◯","","https://appdata.chatwork.com/avatar/2258/2258482.rsz.png"],
+["廣岡 慎史","Consul","Kanda","0年2ヶ月","","◯","https://appdata.chatwork.com/avatar/2474/2474395.rsz.jpg"],
+["長谷川 響馬","Consul","Kanda","0年3ヶ月","","◯","https://appdata.chatwork.com/avatar/2434/2434361.rsz.png"],
+["楢崎 豊","Div1","Kanda","2年3ヶ月","◯","","https://appdata.chatwork.com/avatar/998/998569.rsz.jpg"],
+["塚田 萌","Div1","Hanoi","1年8ヶ月","","◯","https://appdata.chatwork.com/avatar/1325/1325140.rsz.jpg"],
+["山川 信明","Div1","Hanoi","0年4ヶ月","","◯","https://appdata.chatwork.com/avatar/2326/2326979.rsz.jpg"],
+["小野島 一帆","Div1","Hanoi","0年3ヶ月","","◯","https://appdata.chatwork.com/avatar/2326/2326956.rsz.jpg"],
+["安井 春輝","Div1","Hanoi","0年3ヶ月","","◯","https://appdata.chatwork.com/avatar/2162/2162495.rsz.jpg"],
+["Nguyen Van Nhan","Div1","Kanda","1年9ヶ月","","◯","https://appdata.chatwork.com/avatar/2376/2376722.png"],
+["Mai Trung Nghia","Div1","Kanda","1年8ヶ月","","◯","https://appdata.chatwork.com/avatar/611/611513.rsz.jpg"],
+["Dinh Xuan Son","Div1","Kanda","0年5ヶ月","","◯","https://appdata.chatwork.com/avatar/2450/2450961.png"],
+["梶川 雄太","Div2","Kanda","2年6ヶ月","◯","","https://appdata.chatwork.com/avatar/2581/2581433.png"],
+["吉田 慎一","Div2","Kanda","2年3ヶ月","◯","","https://appdata.chatwork.com/avatar/972/972599.rsz.png"],
+["林田 賢行","Div2","Okayama","2年5ヶ月","","◯","https://appdata.chatwork.com/avatar/973/973695.rsz.jpg"],
+["谷口 昌克","Div2","Kanda","0年5ヶ月","","◯","https://appdata.chatwork.com/avatar/2166/2166082.rsz.jpg"],
+["岡崎 拓也","Div2","Hanoi","0年3ヶ月","","◯","https://appdata.chatwork.com/avatar/2109/2109353.png"],
+["Tran Ba Trong","Div2","Kanda","6年4ヶ月","◯","","https://appdata.chatwork.com/avatar/2450/2450961.png"],
+["Vu Thuy Trang","Div2","Kanda","1年7ヶ月","","◯","https://appdata.chatwork.com/avatar/1475/1475138.rsz.jpg"],
+["船木 大郎","Div3","Kanda","1年0ヶ月","◯","","https://appdata.chatwork.com/avatar/1752/1752740.jpg"],
+["茂木 卓也","Div3","Kanda","1年0ヶ月","◯","","https://appdata.chatwork.com/avatar/2339/2339567.rsz.png"],
+["石田 浩章","Design","Hanoi","4年9ヶ月","◯","","https://appdata.chatwork.com/avatar/540/540603.rsz.png"],
+["やばちゃん","","Kanda","0年1ヶ月","","◯","https://appdata.chatwork.com/avatar/540/???.rsz.png"],
+["石川さん（女性）","","Kanda","0年1ヶ月","","◯","https://appdata.chatwork.com/avatar/2597/???.rsz.png"],
+["吉田英子","","Kanda","0年3ヶ月","","◯"],
+["深川 祐史","Design","Hanoi","3年7ヶ月","◯","","https://appdata.chatwork.com/avatar/2561/2561500.png"],
+["杉山 早紀","Design","Hanoi","2年5ヶ月","","◯","https://appdata.chatwork.com/avatar/804/804510.rsz.jpg"],
+["大澤 純子","Design","Hanoi","1年7ヶ月","","◯","https://appdata.chatwork.com/avatar/1361/1361437.rsz.jpg"],
+["鈴木 篤史","Design","Hanoi","1年4ヶ月","","◯","https://appdata.chatwork.com/avatar/1887/1887661.png"],
+["南 慶隆","Design","Hanoi","0年6ヶ月","◯","","https://appdata.chatwork.com/avatar/2404/2404387.png"],
+["勝又 佑一郎","Design","Kanda","0年4ヶ月","","◯","https://appdata.chatwork.com/avatar/2226/2226197.rsz.jpg"]
+]
+
+mentors = data.select{|e| e[4] == "◯"}.shuffle
+mentees = data.select{|e| e[5] == "◯"}.each_with_index.map{|e,i| e << i}
+
+assigned = []
+@groups = []
+mentors.each_with_index do |mentor, i|
+  group = [mentor]
+  rds = mentees.select{|e| !assigned.include?(e[-1]) && e[1] != mentor[1]}.sample((mentees.count+i)/mentors.count)
+  assigned |= rds.map{|e| e[-1]}
+  group |= rds
+  @groups << group
+end
